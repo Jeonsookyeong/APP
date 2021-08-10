b14edfdc25d41365507221f9d30fec03769a9cfc
Jeonsookyeong <21700641@handong.edu>
2021-08-10 17:47:34 +0900

    FAB

--- screens/ProductsOverviewScreen.js
+++ screens/ProductsOverviewScreen.js
@@ -1,33 +1,50 @@
 import React from "react";
-import { FlatList } from "react-native";
-
+import { FlatList, StyleSheet, View } from "react-native";
+import { FAB } from "react-native-elements";
 import PRODUCTS from "../data/dummy-data";
 import ProductItem from "../components/ProductItem";
 
+import Icon from "react-native-vector-icons/Ionicons";
 const ProductsOverviewScreen = (props) => {
   return (
-    <FlatList
-      data={PRODUCTS}
-      keyExtractor={(item) => item.id}
-      renderItem={(itemData) => (
-        <ProductItem
-          image={itemData.item.imageUrl}
-          title={itemData.item.title}
-          createdAt={itemData.item.createdAt}
-          price={itemData.item.price}
-          onSelect={() => {
-            props.navigation.navigate("ProductDetailScreen", {
-              productImage: itemData.item.imageUrl,
-              productOwnerId: itemData.item.ownerId,
-              productTitle: itemData.item.title,
-              productDescription: itemData.item.description,
-              productPrice: itemData.item.price,
-            });
-          }}
-        ></ProductItem>
-      )}
-    />
+    <View style={{ flex: 1 }}>
+      <FlatList
+        data={PRODUCTS}
+        keyExtractor={(item) => item.id}
+        renderItem={(itemData) => (
+          <ProductItem
+            image={itemData.item.imageUrl}
+            title={itemData.item.title}
+            createdAt={itemData.item.createdAt}
+            price={itemData.item.price}
+            onSelect={() => {
+              props.navigation.navigate("ProductDetailScreen", {
+                productImage: itemData.item.imageUrl,
+                productOwnerId: itemData.item.ownerId,
+                productTitle: itemData.item.title,
+                productDescription: itemData.item.description,
+                productPrice: itemData.item.price,
+              });
+            }}
+          ></ProductItem>
+        )}
+      />
+
+      <FAB
+        style={styles.fab}
+        icon={<Icon name="add" size={30} color="white" />}
+
+        placement="right"
+        color="#FF8000"
+      />
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  fab: {
+    paddingBottom: 20,
+  },
+});
 
 export default ProductsOverviewScreen;
