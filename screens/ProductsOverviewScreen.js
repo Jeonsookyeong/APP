792302fd48711c04180cba9ee4c9c36fda2066df
Jeonsookyeong <21700641@handong.edu>
2021-08-11 20:24:17 +0900

    ActionButton

--- screens/ProductsOverviewScreen.js
+++ screens/ProductsOverviewScreen.js
@@ -1,49 +1,64 @@
-import React from "react";
+import React, { Component } from "react";
 import { FlatList, StyleSheet, View } from "react-native";
-import { FAB } from "react-native-elements";
 import PRODUCTS from "../data/dummy-data";
 import ProductItem from "../components/ProductItem";
 
+import ActionButton from "@logvinme/react-native-action-button";
 import Icon from "react-native-vector-icons/Ionicons";
+
 const ProductsOverviewScreen = (props) => {
   return (
-    <View style={{ flex: 1 }}>
-      <FlatList
-        data={PRODUCTS}
-        keyExtractor={(item) => item.id}
-        renderItem={(itemData) => (
-          <ProductItem
-            image={itemData.item.imageUrl}
-            title={itemData.item.title}
-            createdAt={itemData.item.createdAt}
-            price={itemData.item.price}
-            onSelect={() => {
-              props.navigation.navigate("ProductDetailScreen", {
-                productImage: itemData.item.imageUrl,
-                productOwnerId: itemData.item.ownerId,
-                productTitle: itemData.item.title,
-                productDescription: itemData.item.description,
-                productPrice: itemData.item.price,
-              });
-            }}
-          ></ProductItem>
-        )}
-      />
+    <View style={{ flex: 1, backgroundColor: "#f3f3f3" }}>
+      {
+        /* Rest of the app comes ABOVE the action button component !*/
+        <FlatList
+          data={PRODUCTS}
+          keyExtractor={(item) => item.id}
+          renderItem={(itemData) => (
+            <ProductItem
+              image={itemData.item.imageUrl}
+              title={itemData.item.title}
+              createdAt={itemData.item.createdAt}
+              price={itemData.item.price}
+              onSelect={() => {
+                props.navigation.navigate("ProductDetailScreen", {
+                  productImage: itemData.item.imageUrl,
+                  productOwnerId: itemData.item.ownerId,
+                  productTitle: itemData.item.title,
+                  productDescription: itemData.item.description,
+                  productPrice: itemData.item.price,
+                });
+              }}
+            ></ProductItem>
+          )}
+        />
+      }
+      <ActionButton buttonColor="#FF8000">
+        <ActionButton.Item
+          buttonColor="#FF8000"
+          title="동네홍보"
+          onPress={() => {}}
+        >
+          <Icon name="newspaper" style={styles.actionButtonIcon} />
+        </ActionButton.Item>
 
-      <FAB
-        style={styles.fab}
-        icon={<Icon name="add" size={30} color="white" />}
-
-        placement="right"
-        color="#FF8000"
-      />
+        <ActionButton.Item
+          buttonColor="#FF8000"
+          title="중고거래"
+          onPress={() => {}}
+        >
+          <Icon name="pencil" style={styles.actionButtonIcon} />
+        </ActionButton.Item>
+      </ActionButton>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  fab: {
-    paddingBottom: 20,
+  actionButtonIcon: {
+    fontSize: 20,
+    height: 22,
+    color: "white",
   },
 });
 
